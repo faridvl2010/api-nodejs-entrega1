227df729d16775540ffdebd3ab605577ff7852ce
faridvl2010 <faridvl2010@gmail.com>
2022-09-12 18:33:45 -0500

    Agregando correctamente

--- routes.js
+++ routes.js
@@ -1,27 +1,49 @@
 const express = require('express')
 const routes = express.Router()
-routes.get('/', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+//TRAER USUARIO
+routes.get('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT * FROM users.new_table;', (err, rows)=>{
-            if(err) return res.send(err)
+        conn.query('SELECT * FROM users.new_table;', (err, rows) => {
+            if (err) return res.send(err)
 
             res.json(rows)
         })
     })
 })
-
-routes.post('/', (req, res)=>{
+//INSTERAR NUEVO 
+routes.post('/', (req, res) => {
     console.log(req.body)
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
-        conn.query('INSERT INTO users.new_table set ?', [req.body], (err, rows)=>{
-            if(err) return res.send(err)
-
-            res.send('user added!')
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('INSERT INTO user set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err)
+            res.send('user add')
         })
     })
 })
+//Actualizar usuario
+routes.put('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('UPDATE put set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err)
+            res.send('user updated!')
+        })
+    })
+})
+//ACTUALIZAR ESTADO
+routes.patch('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('UPTADE user set ? WHERE id = ?', [req.body, id], (err, rows) => {
+            if (err) return res.send(err)
+            res.send('inactive user')
+        })
+    })
+})
+
 
 module.exports = routes
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const mysql = require('mysql')
 const myconnection = require('express-myconnection')
 const app = express()
 const routes = require('./routes')
+
 app.set('port', 9000)
+
+
 const dbOptions = {
     host: 'localhost',
     port: 3306,
@@ -12,11 +15,12 @@ const dbOptions = {
     database: 'users'
 }
 app.use(myconnection(mysql, dbOptions, 'single'))
+app.use(express.json())
 //route
 app.get('/', (req, res) => {
     res.send('Welcome to my APIREST')
 })
-app.use('/api',routes)
+app.use('/api', routes)
 //Runnig
 app.listen(app.get('port'), () => {
     console.log('server running on port ', app.get('port'))

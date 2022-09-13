f7a62bc72760c04c46941fcbbdee7b170f8d35b5
faridvl2010 <faridvl2010@gmail.com>
2022-09-13 10:42:03 -0500

    ahora si 4 de 4

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ routes.get('/', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.send(err)
 
-        conn.query('SELECT * FROM users.new_table;', (err, rows) => {
+        conn.query('SELECT * FROM user;', (err, rows) => {
             if (err) return res.send(err)
 
             res.json(rows)

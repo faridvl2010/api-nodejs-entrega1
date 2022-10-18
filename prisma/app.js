e3d3ce53485728eef77e5f25b9302c32a5c3b80c
SergioV19 <sergiovar19@gmail.com>
2022-10-18 11:18:15 -0500

    configuracion de insericiones

--- prisma/app.js
+++ prisma/app.js
@@ -3,12 +3,17 @@ const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main(){
-    const post = await prisma.post.create({
+    const post = await prisma.usuarios.create({
         data:{
-            title: 'Titulo 1',
-            content: ''
+            NAME: "Farid",
+            LAST_NAME: "Valiente",
+            TYPE_DOCUMENT: "ti",
+            DOCUMENT: "1010152996",
+            STATE: "a",
+            CREATION_DATE: new Date("2021-12-31 23:59:59")
         }
     })
+    console.log(post)
 }
 
 main()

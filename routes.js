f5bcd627589b5e07bffd20384e6694db39941b4c
faridvl2010 <faridvl2010@gmail.com>
2022-10-24 18:40:09 -0500

    sign jwt

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
+const jwt  = require("jsonwebtoken")
 routes.use(express.json())
 
 //TRAER USUARIO
@@ -47,6 +48,43 @@ routes.patch('/:id', (req, res) => {
         })
     })
 })
+//SIGN
+routes.post('/api/login', async (req, res) => {
+    const{NAME, LAST_NAME, EMAIL} = req.body
+    const post = await prisma.usuarios.create({
+        
+        data:{
+            NAME, LAST_NAME, EMAIL
+        }
+    })
+    jwt.sign({data},'secretKey',{expiresIn: '60s'},(err,token)=>{
+        res.json({
+            token
+        })
+    })
+    console.log(post)
+})
 
-
+//authorizacion: Bearer <token>
+function verifyToken(req,res,next){
+    const bearerHeader= req.headers['authorizacion']
+    if (typeof bearerHeader != 'undefined') {
+        const bearerToken  = bearerHeader.split(" ")[1]
+        req.token = bearerToken
+        next()
+    }else{
+        res.sendStatus(403)
+    }
+}
+routes.post("/api/post",verifyToken,(req , res)=>{
+    jwt.verify(req.token,'secretKey',(error , authData)=>{
+        if (error) {
+            res.sendStatus(403)
+        }else{
+            res.json({
+                authData
+            })
+        }
+    })
+})
 module.exports = routes

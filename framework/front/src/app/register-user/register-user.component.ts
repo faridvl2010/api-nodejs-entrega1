ebede2f8b659d8acb3fde2c363f4e958d43c8f24
faridvl2010 <faridvl2010@gmail.com>
2022-11-06 13:25:37 -0500

    login y registro listo

--- framework/front/src/app/register-user/register-user.component.ts
+++ framework/front/src/app/register-user/register-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  volverHome(){
+    this.router.navigate(['home'])
   }
 
 }

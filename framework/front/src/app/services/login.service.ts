565e1df74a81c4385c1e956870b507fa3012a184
faridvl2010 <faridvl2010@gmail.com>
2022-11-14 21:21:22 -0500

    .

--- framework/front/src/app/services/login.service.ts
+++ framework/front/src/app/services/login.service.ts
@@ -1,18 +1,61 @@
 import { GetUser } from './../models/UsuarioLog';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import{UserLogin, Usuario} from'./../models/Usuario';
+import { BehaviorSubject, catchError, map, Observable, throwError } from 'rxjs';
+import{JwtHelperService} from '@auth0/angular-jwt'
+const helper = new JwtHelperService()
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  URL_API = 'http://localhost:3000/api/'
+  private loggedIn = new BehaviorSubject<boolean>(false)
+  URL_API = 'http://localhost:3000/auth/login'
 
-  constructor(private http: HttpClient) { }
-  getUser(){
-    return this.http.get(this.URL_API)
+  constructor(private http: HttpClient) {
+    this.checkToken()
+   }
+get isLogged():Observable<boolean>{
+  return this.loggedIn.asObservable()
+}
+  Login(authData:UserLogin): Observable<void|Usuario>{
+    return this.http
+    .post<Usuario>(this.URL_API,authData)
+    .pipe(
+      map((res:Usuario)=>{
+        this.saveToken(res.PASSWORD)
+        this.loggedIn.next(true)
+        return res;
+      }),
+      catchError((err)=>this.handlerError(err))
+    )
   }
-  registerLogin(EMAIL: String, PASSWORD:String){
-   
+  logout(){
+    localStorage.removeItem('PASSWORD')
+    this.loggedIn.next(false)
+
+  }
+  private checkToken(){
+    const UserToken =  localStorage.getItem('PASSWORD')
+    const isExpired = helper.isTokenExpired('PASSWORD')
+    console.log('isExpired',isExpired)
+    if(isExpired){
+      this.logout()
+    }else{
+      this.loggedIn.next(true)
+    }
+    //set userLogged
+  }
+  private saveToken(PASSWORD: string){
+    localStorage.setItem('PASSWORD',PASSWORD)
+  }
+  
+  private handlerError(err:any):Observable<never>{
+    let errorMessage = `Error de datos`
+    if (err) {
+      errorMessage=`Error:code ${err.message}`
+    }
+    window.alert(errorMessage)
+    return throwError(errorMessage)
   }
 }
